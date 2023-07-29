555f2d4ec727f490a995e6255fa9d19bf1435ee5
atusi0314 <130933944+atusi0314@users.noreply.github.com>
2023-07-29 08:13:11 +0000

    [finish]3-15

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,12 +1,14 @@
 class BooksController < ApplicationController
-  def new
-    @book = Book.new
-  end
 
   def create
-    book = Book.new(book_params)
-    book.save
-    redirect_to books_path
+   @books = Book.new(book_params)
+    if @books.save
+      flash[:notice] = "Book was successfully created."
+      redirect_to book_path(@books.id)
+    else
+      @book = Book.all
+      render :index
+    end
   end
 
   def index
@@ -19,6 +21,23 @@ class BooksController < ApplicationController
   end
 
   def edit
+    @book = Book.find(params[:id])
+  end
+
+  def update
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      flash[:notice] = "Book was successfully updated."
+      redirect_to book_path(@book.id)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to books_path
   end
 
   private
